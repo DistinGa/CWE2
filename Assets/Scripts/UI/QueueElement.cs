f28a977841ab7aeb7a1d4bbc385987fb65c805a8
Дмитрий Вавилов <dvav@mail.ru>
2020-01-23 16:59:18 +0700

    Департамент исследования.

---
+++ Assets/Scripts/UI/QueueElement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class QueueElement : MonoBehaviour, IDragHandler
+{
+    [SerializeField] bool Active = true;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (Active)
+        {
+            var underElement = eventData.pointerCurrentRaycast.gameObject.GetComponentInParent<QueueElement>();
+            if (underElement != null)
+                transform.SetSiblingIndex(eventData.pointerCurrentRaycast.gameObject.GetComponentInParent<QueueElement>().transform.GetSiblingIndex());
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
