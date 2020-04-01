c295e49b927a7e3a4dac3e0dc3fabec4fc64ea41
Дмитрий Вавилов <dvav@mail.ru>
2020-04-01 13:55:36 +0700

    Коммит перед оплатой 01/03/20

---
+++ Assets/Scripts/UI/BattleCard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BattleCard : MonoBehaviour
+{
+    [SerializeField] Image UnitImage, Flag;
+    [SerializeField] Image SupplyIndicator, EngineIndicator;
+    [SerializeField] Text UnitName, UnitVersion;
+    [SerializeField] Text Amount;
+    [SerializeField] GameObject UserControlButton, AIControlButton;
+
+    bool _userControl;
+
+    public float Supply
+    {
+        set { SupplyIndicator.fillAmount = value; }
+    }
+
+    public float Engine
+    {
+        set { EngineIndicator.fillAmount = value; }
+    }
+
+    public string Amounts
+    {
+        set { Amount.text = value; }
+    }
+
+    public bool UserControl
+    {
+        get { return _userControl; }
+
+        set
+        {
+            _userControl = value;
+            UserControlButton.SetActive(_userControl);
+            AIControlButton.SetActive(!_userControl);
+        }
+    }
+
+    public void Init(string name, string version, Sprite flag, Sprite Image)
+    {
+        UnitName.text = name;
+        UnitVersion.text = version;
+        Flag.sprite = flag;
+        UnitImage.sprite = Image;
+    }
+}
---
+++ Assets/Scripts/UI/WarPhasePanel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WarPhasePanel : MonoBehaviour
+{
+    [SerializeField] GameObject Image, InactiveImage;
+    [SerializeField] GameObject StartBattleButton, GoToBattleButton, VictoryImage;
+    [SerializeField] GameObject BonusPenaltyPrefab;
+    [SerializeField] Transform Bonuses;
+}
