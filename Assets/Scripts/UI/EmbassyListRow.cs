f4552cb6ccd55132b3972caeb1046c81e18a1774
Дмитрий Вавилов <dvav@mail.ru>
2020-04-21 01:25:03 +0700

    Посольство.

---
+++ Assets/Scripts/UI/EmbassyListRow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmbassyListRow : MonoBehaviour
+{
+    public Image flag, regimeIcon, focusIcon;
+    public Text countryName, prosperity, points;
+    public GameObject warButton, SelectBack;
+
+    public int RegID;
+
+    EmbassyUI EmbassyUI;
+
+    public void SelectCountry()
+    {
+        EmbassyUI.SelectCountry(RegID);
+    }
+
+}
---
+++ Assets/Scripts/UI/EmbassyUI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmbassyUI : MonoBehaviour
+{
+    int curRegID;
+
+    public void SelectCountry(int regID)
+    {
+
+    }
+
+    /// <summary>
+    /// Сортировка по стране.
+    /// </summary>
+    public void SortByName()
+    {
+
+    }
+
+    /// <summary>
+    /// Сортировка по режиму, в порядке индексов.
+    /// </summary>
+    public void SortByRegime()
+    {
+
+    }
+
+    /// <summary>
+    /// Сортировка по величине Prosperity в порядке убывания.
+    /// </summary>
+    public void SortByProsperity()
+    {
+
+    }
+
+    /// <summary>
+    /// Сортировка по очкам в порядке убывания.
+    /// </summary>
+    public void SortByPoints()
+    {
+
+    }
+
+    /// <summary>
+    /// Сортировка по фокусамм.
+    /// </summary>
+    public void SortByFocus()
+    {
+
+    }
+
+    /// <summary>
+    /// Сортировака по состоянию войны (сначала с войной).
+    /// </summary>
+    public void SortByWar()
+    {
+
+    }
+
+}
